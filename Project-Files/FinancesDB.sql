cdf943be02b92ace5879685343bbd5c601ef4fc3
Levi Spellmeyer <levi.spellmeyer.04@gmail.com>
2025-03-04 12:36:26 -0600

    Update FinancesDB.sql to include Table for entries in the database

--- Project-Files/FinancesDB.sql
+++ Project-Files/FinancesDB.sql
@@ -1,1 +1,11 @@
+CREATE TABLE tblEntries(
+  entryID INT AUTO_INCREMENT NOT NULL,
+  dateOfTransaction DATETIME NOT NULL,
+  amouont INT NOT NULL,
+  location VARCHAR(50),
+  CreditDebit VARCHAR(10),
+  userID INT,
+
+  PRIMARY KEY(entryID)
+);
 
