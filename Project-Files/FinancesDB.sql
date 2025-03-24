11759ec11222328184202674bfb0b029992c6fdb
L-Spellmeyer <156337140+L-Spellmeyer@users.noreply.github.com>
2025-03-24 13:07:20 -0500

    Draft of what the database i will include will look like

--- Project-Files/FinancesDB.sql
+++ Project-Files/FinancesDB.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tblEntries(
   entryID INT AUTO_INCREMENT NOT NULL,
-  dateOfTransaction DATETIME NOT NULL,
-  amouont INT NOT NULL,
+  dateOfTransaction DATE NOT NULL,
+  amount DECIMAL(10,2) NOT NULL,
   location VARCHAR(50),
   CreditDebit VARCHAR(10),
   userID INT,
@@ -9,3 +9,12 @@ CREATE TABLE tblEntries(
   PRIMARY KEY(entryID)
 );
 
+
+CREATE TABLE tblBalance(
+	balanceID VARCHAR(10),
+	Current_Bal DECIMAL(10,2),
+    
+    PRIMARY KEY(balanceID)
+);
+
+-- Note that when i insert a new entry i will have to check if account type == credit or debit and then update balance
